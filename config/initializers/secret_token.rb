309210643d81e82fe8a7f93a0d5a35bd10809119
puzanov <puzanov@gmail.com>
2011-06-20 22:15:02 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brand::Application.config.secret_token = 'b73451ead2b00375e350e7215f69e5244f67b9a73e1a63c50dceba429ef014e8864d6c1d6cd148130e0602afa61048d150aa54d773b141ee06a20dfea11fb46a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brand::Application.config.session_store :cookie_store, :key => '_brand_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brand::Application.config.session_store :active_record_store
