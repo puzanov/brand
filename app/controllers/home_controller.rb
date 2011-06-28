6632e8802c8bae5cf84d614c50721e4bb7ed13cb
puzanov <puzanov@gmail.com>
2011-06-28 17:58:15 +0600

    working with news on main page (layout)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @articles = Article.order("id desc").limit(2);
+    @short_articles = Article.order("id desc").limit(5).offset(2);
   end
 end
