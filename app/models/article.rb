140f7f56775aeb4403dc600d1423c4c0a4e7aa53
puzanov <puzanov@gmail.com>
2011-06-28 17:32:41 +0600

    validation in articles is enabled

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  validates_presence_of :title,   :message => "Нужно обязательно указать заголовок"
+  validates_length_of   :brief,   :minimum => 6, :message => "Не указано описание или оно сликом короткое"
+  validates_length_of   :content, :minimum => 6, :message => "Не указан полный текст или он слишком короткий"
 end
