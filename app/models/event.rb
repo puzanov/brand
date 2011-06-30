2558965129592f2d35bc24d89db2c48ac5cbdbad
puzanov <puzanov@gmail.com>
2011-06-30 23:43:48 +0600

    event validation is done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+  validates_presence_of :title,   :message => "Нужно обязательно указать заголовок"
+  validates_length_of   :content,   :minimum => 6, :message => "Не указано описание или оно сликом короткое"
 end
