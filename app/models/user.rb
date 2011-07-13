553f8d1b0472560d4a82daff3c62a2bba67447ef
Tim Isakjanov <tim.isakjanov@gmail.com>
2011-07-13 09:12:14 +0600

    Валидация пользователя

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates_presence_of :login,     :message => "Необходимо указать логин пользователя"
+  validates_presence_of :password,  :message => "Необходимо указать пароль пользователя"
+  validates_presence_of :name,      :message => "Необходимо указать имя пользователя"
+  validates_presence_of :email,     :message => "Необходимо указать email пользователя"
+
+  validates_uniqueness_of :login,   :message => "Такой логин уже существует"
 end
