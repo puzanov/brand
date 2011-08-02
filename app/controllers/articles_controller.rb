1026df419c638e04f4144679a3ff37b940fafca2
Tim Isakjanov <tim.isakjanov@gmail.com>
2011-08-02 18:13:39 +0600

    fix problem with session when uploading image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_filter :only_for_admin , :except => [:index, :show, :upload_img]
+  protect_from_forgery :except => :upload_img
 
   def upload_img
     uploaded_io = params[:nicImage]
@@ -14,10 +15,6 @@ class ArticlesController < ApplicationController
     status["width"] = image.columns
     status["url"] = "/uploads/" + current_ts + uploaded_io.original_filename
     json_status = status.to_json
-
-    logger.info '!!!!!!!!!!!!!!11'
-    logger.info session.inspect
-
     render :text => "<script>try{top.nicUploadButton.statusCb(#{json_status})}catch(e){alert(e.message);}</script>"
   end
   
@@ -60,8 +57,6 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
-    logger.info "!!!!!!!!!!!!"
-    logger.info session.inspect
     @article = Article.find(params[:id])
   end
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,5 +4,5 @@ Brand::Application.config.session_store :cookie_store, :key => '_brand_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Brand::Application.config.session_store :active_record_store
+# (create the session table with ":rails generate session_migration")
+# Brand::Application.config.session_store :active_record_store, :key => '_brand_session'
